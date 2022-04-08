20e08c101cc7deac9d20bef2a88e034bccbc7f98
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-04-08 17:29:28 +0800

    Панель администратора

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.account_panel')
+@section('title', 'Главная')
+@section('panel', "Панель администратора")
+@section('keywords', 'keywords')
+@section('description', 'description')
+@section('canonical', route('index'))
+
+
+@section('content')
+    
+<div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>150</h3>
+
+                <p>Новые записи</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-clipboard-list"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53</h3>
+
+                <p>Портфолио</p>
+              </div>
+              <div class="icon">
+                <i class="far fa-image"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Блог</p>
+              </div>
+              <div class="icon">
+                <i class="fab fa-blogger-b"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p>Клиенты</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>  
+</div>   
+  
+@endsection
