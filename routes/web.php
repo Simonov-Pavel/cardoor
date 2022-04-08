33a4e64b2a2dea7fbbbea4cc920e91dea8269c88
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-04-08 15:09:32 +0800

    Добавление маршрутов Admin and Account

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,13 @@ require __DIR__.'/auth.php';
 Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('get.logout');
 
 Route::get('/', [MainController::class, 'index'])->name('index');
+Route::middleware(['auth'])->group(function(){
+    Route::resources(['user' => UserController::class,]);
+    Route::group(['prefix'=>'account'], function(){
+        Route::get('/', [App\Http\Controllers\Account\MainController::class, 'index'])->name('account');
+    });
+
+    Route::group(['prefix'=>'admin', 'middleware'=>['admin']], function(){
+        Route::get('/', [App\Http\Controllers\Admin\MainController::class, 'index'])->name('admin');
+    });
+});
