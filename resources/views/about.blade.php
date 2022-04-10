ec7207c6f4a80d574601abbe4ab1f8b6c54138a2
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-04-10 22:00:36 +0800

    about.blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,20 +5,8 @@
 @section('canonical', route('index'))
 
 @section('content')
-<section id="page-title-area" class="section-padding overlay">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section-title  text-center">
-                        <h2>About US</h2>
-                        <span class="title-line"><i class="fa fa-car"></i></span>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
+@include('includes.header-page')
     <!--== About Page Content Start ==-->
     <section id="about-area" class="section-padding">
         <div class="container">
@@ -163,139 +151,7 @@
     </section>
     <!--== Our Facility Content End ==-->
 
-    <!--== Partner Area Start ==-->
-    <div id="partner-area">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="partner-content-wrap">
-                        <!-- Single Partner Start -->
-                        <div class="single-partner">
-                            <div class="display-table">
-                                <div class="display-table-cell">
-                                    <img src="assets/img/partner/partner-logo-1.png" alt="JSOFT">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Partner End -->
+    @include('includes.partner')
+    @include('includes.testimonials')
 
-                        <!-- Single Partner Start -->
-                        <div class="single-partner">
-                            <div class="display-table">
-                                <div class="display-table-cell">
-                                    <img src="assets/img/partner/partner-logo-2.png" alt="JSOFT">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Partner End -->
-
-                        <!-- Single Partner Start -->
-                        <div class="single-partner">
-                            <div class="display-table">
-                                <div class="display-table-cell">
-                                    <img src="assets/img/partner/partner-logo-3.png" alt="JSOFT">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Partner End -->
-
-                        <!-- Single Partner Start -->
-                        <div class="single-partner">
-                            <div class="display-table">
-                                <div class="display-table-cell">
-                                    <img src="assets/img/partner/partner-logo-4.png" alt="JSOFT">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Partner End -->
-
-                        <!-- Single Partner Start -->
-                        <div class="single-partner">
-                            <div class="display-table">
-                                <div class="display-table-cell">
-                                    <img src="assets/img/partner/partner-logo-5.png" alt="JSOFT">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Partner End -->
-
-                        <!-- Single Partner Start -->
-                        <div class="single-partner">
-                            <div class="display-table">
-                                <div class="display-table-cell">
-                                    <img src="assets/img/partner/partner-logo-1.png" alt="JSOFT">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Partner End -->
-
-                        <!-- Single Partner Start -->
-                        <div class="single-partner">
-                            <div class="display-table">
-                                <div class="display-table-cell">
-                                    <img src="assets/img/partner/partner-logo-4.png" alt="JSOFT">
-                                </div>
-                            </div>
-                        </div>
-                        <!-- Single Partner End -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--== Partner Area End ==-->
-
-    <!--== Testimonials Area Start ==-->
-    <section id="testimonial-area" class="section-padding">
-        <div class="container">
-            <div class="row">
-                <!-- Section Title Start -->
-                <div class="col-lg-12">
-                    <div class="section-title  text-center">
-                        <h2>Testimonials</h2>
-                        <span class="title-line"><i class="fa fa-car"></i></span>
-                        <p>Lorem ipsum dolor sit amet elit.</p>
-                    </div>
-                </div>
-                <!-- Section Title End -->
-            </div>
-
-            <div class="row">
-                <div class="col-lg-8 col-md-12 m-auto">
-                    <div class="testimonial-content">
-                        <!--== Single Testimoial Start ==-->
-                        <div class="single-testimonial">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis totam obcaecati impedit, at autem repellat vel magni architecto veritatis sed.</p>
-                            <h3>Vongchong Smith</h3>
-                            <div class="client-logo">
-                                <img src="assets/img/client/client-pic-1.jpg" alt="JSOFT">
-                            </div>
-                        </div>
-                        <!--== Single Testimoial End ==-->
-
-                        <!--== Single Testimoial Start ==-->
-                        <div class="single-testimonial">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis totam obcaecati impedit, at autem repellat vel magni architecto veritatis sed.</p>
-                            <h3>Amader Tuni</h3>
-                            <div class="client-logo">
-                                <img src="assets/img/client/client-pic-3.jpg" alt="JSOFT">
-                            </div>
-                        </div>
-                        <!--== Single Testimoial End ==-->
-
-                        <!--== Single Testimoial Start ==-->
-                        <div class="single-testimonial">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis totam obcaecati impedit, at autem repellat vel magni architecto veritatis sed.</p>
-                            <h3>Atex Tuntuni Smith</h3>
-                            <div class="client-logo">
-                                <img src="assets/img/client/client-pic-2.jpg" alt="JSOFT">
-                            </div>
-                        </div>
-                        <!--== Single Testimoial End ==-->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--== Testimonials Area End ==-->
     @endsection
---
+++ resources/views/includes/header-page.blade.php
@@ -0,0 +1,13 @@
+<section id="page-title-area" class="section-padding overlay">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title  text-center">
+                    <h2>About US</h2>
+                    <span class="title-line"><i class="fa fa-car"></i></span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
