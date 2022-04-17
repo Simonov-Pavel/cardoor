ddd8e5afdfbc645ad0c701838574c3b9aac1a7cc
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-04-17 14:32:02 +0800

    add resources/views/admin/contact/index.blade.php

--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -18,31 +18,42 @@
 </div>
 @endsection
 
-
 @section('content') 
 
+<a href="{{ route('contact.store', $contact) }}" class="btn btn-primary ml-2 mb-3">Редактировать</a>
+@if(isset($contact))
 <table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Адрес</th>
-      <th scope="col">Телефон</th>
-      <th scope="col">E-mail</th>
-      <th scope="col">Время работы</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
   <tbody>
-    
     <tr>
-      <th scope="row"></th>
-      <th scope="row"></th>
-      <th scope="row"></th>
-      <th scope="row"></th>
-      <td scope="row" class="d-flex align-items-center">
-        <a href="" class="btn btn-tool" title="Редактировать"><i class="fas fa-pen text-primary"></i></a>
+      <td>Логотип</td>
+      <td>
+        <picture>
+          <source srcset="{{ Storage::url('contact/'.$contact->logo_webp) }}" type="image/webp">
+          <img src="{{ Storage::url('contact/'.$contact->logo) }}" width="100" alt="logo">
+        </picture>
       </td>
     </tr>
-    
+    <tr>
+      <td>Адрес</td>
+      <td>{{$contact->address}}</td>
+    </tr>
+    <tr>
+      <td>Телефон</td>
+      <td>{{$contact->phone}}</td>
+    </tr>
+    <tr>
+      <td>Email</td>
+      <td>{{$contact->email}}</td>
+    </tr>
+    <tr>
+      <td>Описание</td>
+      <td>{!!$contact->description!!}</td>
+    </tr>
   </tbody>
 </table>
+@else
+<h3 class='text-center'>Вы пока ничего не добавили</h3>
+@endif
+
+</div>
 @endsection
