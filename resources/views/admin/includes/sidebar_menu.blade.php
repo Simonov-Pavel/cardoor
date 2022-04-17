0cdfaa26bda3bc4541e93876898e9f8c84ac745e
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-04-17 13:08:22 +0800

    add route('contact.index')

--- resources/views/admin/includes/sidebar_menu.blade.php
+++ resources/views/admin/includes/sidebar_menu.blade.php
@@ -4,7 +4,7 @@
     </a>
 </li>
 <li class="nav-item">
-    <a href="" class="nav-link @menuactive('contact.*')">
+    <a href="{{ route('contact.index') }}" class="nav-link @menuactive('contact.*')">
         <i class="nav-icon fa fa-map-marker"></i><p>Контакты</p>
     </a>
 </li>
