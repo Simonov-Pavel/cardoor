8f68158394e7c8d3081ed52450c3a9b862d74a90
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-04-17 11:47:20 +0800

    admin sidebar-menu

---
+++ resources/views/admin/includes/sidebar_menu.blade.php
@@ -0,0 +1,10 @@
+<li class="nav-item">
+    <a href="{{ route('admin') }}" class="nav-link @menuactive('admin')">
+        <i class="nav-icon fas fa-home"></i><p>Главная</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="" class="nav-link @menuactive('contact.*')">
+        <i class="nav-icon fa fa-map-marker"></i><p>Контакты</p>
+    </a>
+</li>
