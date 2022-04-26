ca04e8e53d62b2bc8a49989700ccb0798e5b3366
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-04-26 15:31:38 +0800

    Закрыл от индексации регистрацию и личный кабинет

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -106,34 +106,36 @@
                     </div>
                     <!--== Logo End ==-->
 
+                    <!--noindex-->
                     <div class="cart-icon col-lg-2 text-right">
-                            <a href="" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                @guest
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            @guest
                                 <i class="fa fa-user mr-2" aria-hidden="true" style="font-size:30px;"></i>Акаунт
-                                @endguest
-                                @auth
-                                    <picture>
-                                        <sourse srcset="{{ Storage::url('user/' . Auth::user()->img_preview_webp)}}" type="image/webp">
-                                        <img src="{{ Storage::url('user/' . Auth::user()->img_preview)}}" alt="User Image - {{Auth::user()->name}}" style="width:45px; border-radius:50%;display:inline-block">
-                                    </picture>
+                            @endguest
+                            @auth
+                                <picture>
+                                    <sourse srcset="{{ Storage::url('user/' . Auth::user()->img_preview_webp)}}" type="image/webp">
+                                    <img src="{{ Storage::url('user/' . Auth::user()->img_preview)}}" alt="User Image - {{Auth::user()->name}}" style="width:45px; border-radius:50%;display:inline-block">
+                                </picture>
                                     
-                                    {{Auth::user()->name}}
-                                @endauth
-                            </a>
-                            <ul class="dropdown-menu" style="left:30px;@auth @admin width:250px; @endadmin @endauth">
-                                @guest
-                                <li><a href="{{ route('register') }}" class="account-link ml-2">Регистрация</a></li>
-                                <li><a href="{{ route('login') }}" class="account-link ml-2">Войти</a></li>
-                                @endguest         
-                                @auth
-                                    <li><a href="{{ route('account') }}" class="account-link ml-2">Личный кабинет</a></li>
-                                    @admin 
-                                        <li><a href="{{ route('admin') }}" class="account-link ml-2">Панель администратора</a></li>
-                                    @endadmin
-                                    <li><a href="{{ route('get.logout') }}" class="account-link ml-2">Выйти</a></li>
-                                @endauth
-                            </ul>
-                        </div>
+                                {{Auth::user()->name}}
+                            @endauth
+                        </a>
+                        <ul class="dropdown-menu" style="left:30px;@auth @admin width:250px; @endadmin @endauth">
+                            @guest
+                                <li><a href="{{ route('register') }}" rel="nofollow" class="account-link ml-2">Регистрация</a></li>
+                                <li><a href="{{ route('login') }}" rel="nofollow" class="account-link ml-2">Войти</a></li>
+                            @endguest         
+                            @auth
+                                <li><a href="{{ route('account') }}" rel="nofollow" class="account-link ml-2">Личный кабинет</a></li>
+                                @admin 
+                                    <li><a href="{{ route('admin') }}" rel="nofollow" class="account-link ml-2">Панель администратора</a></li>
+                                @endadmin
+                                <li><a href="{{ route('get.logout') }}" rel="nofollow" class="account-link ml-2">Выйти</a></li>
+                            @endauth
+                        </ul>
+                    </div>
+                    <!--/noindex-->
 
                     <!--== Main Menu Start ==-->
                     <div class="col-lg-8 d-none d-lg-block">
