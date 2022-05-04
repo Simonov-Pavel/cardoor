5001be597fa562c7a80c646d76ee1aa5dc5ab70a
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-04 16:22:40 +0800

    Перенес сервис Images в папку Service

--- app/Http/Controllers/Admin/AdminContactController.php
+++ app/Http/Controllers/Admin/AdminContactController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use App\Http\Requests\Admin\ContactRequest;
-use App\Classes\Images;
+use App\Services\Images;
 
 class AdminContactController extends Controller
 {
