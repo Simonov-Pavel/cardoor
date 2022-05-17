c3ca4f836c03c02e2419344855c6eba93bd5942c
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-17 13:08:01 +0800

    Создание модели и миграции раздела о нас

---
+++ database/migrations/2022_05_17_045944_create_abouts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->string('conditions_header');
+            $table->text('conditions_rental');
+            $table->text('video');
+            $table->string('img');
+            $table->string('img_webp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
