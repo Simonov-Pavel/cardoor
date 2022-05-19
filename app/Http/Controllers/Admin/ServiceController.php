ddcb40743055240719f8cbcc41b3bd82f35f5e6a
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-19 17:40:54 +0800

    create controller and request Service

---
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Service;
+use App\Http\Requests\Admin\ServiceRequest;
+
+class ServiceController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(ServiceRequest $request)
+    {
+        //
+    }
+
+    public function show(Service $service)
+    {
+        //
+    }
+
+    public function edit(Service $service)
+    {
+        //
+    }
+
+    public function update(ServiceRequest $request, Service $service)
+    {
+        //
+    }
+
+    public function destroy(Service $service)
+    {
+        //
+    }
+}
