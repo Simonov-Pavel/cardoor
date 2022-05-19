8b27d7f106df63f7e81720e9d367ea53b9ff6e05
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-19 14:56:19 +0800

    clear views

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,22 +8,9 @@
 @section('content')
 
 @include('includes.header-page')
-    <!--== About Page Content Start ==-->
     <section id="about-area" class="section-padding">
         <div class="container">
             <div class="row">
-                <!-- Section Title Start -->
-                <div class="col-lg-12">
-                    <div class="section-title  text-center">
-                        <h2>О нас</h2>
-                        <span class="title-line"><i class="fa fa-car"></i></span>
-                    </div>
-                </div>
-                <!-- Section Title End -->
-            </div>
-
-            <div class="row">
-                <!-- About Content Start -->
                 <div class="col-lg-6">
                     <div class="display-table">
                         <div class="display-table-cell">
@@ -57,7 +44,6 @@
             </div>
         </div>
     </section>
-    <!--== Our Facility Content End ==-->
 
     @include('includes.partner')
     @include('includes.testimonials')
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -8,39 +8,19 @@
 @section('content')
 
 @include('includes.header-page')
-    <!--== About Page Content Start ==-->
-    <section id="about-area" class="section-padding">
+    <section class="section-padding">
         <div class="container">
             <div class="row">
-                <!-- Section Title Start -->
                 <div class="col-lg-12">
                     <div class="section-title  text-center">
-                        <h2>Наши услуги</h2>
+                        <h2>Мы предоставляем широкий спектр услуг</h2>
                         <span class="title-line"><i class="fa fa-car"></i></span>
                     </div>
                 </div>
-                <!-- Section Title End -->
             </div>
 
-            <div class="row">
-                <h3>Мы предоставляем широкий спектр услуг</h3>
-            </div>
         </div>
     </section>
-    
-    <section id="our-facility" class="section-padding overlay">
-        <div class="container">
-            <div class="row">
-                <div class="section-title text-center">
-                    <h2></h2>
-                </div>
-                <div class="single-our-facility">
-                    
-                </div> 
-            </div>
-        </div>
-    </section>
-    <!--== Our Facility Content End ==-->
 
     @include('includes.partner')
     @include('includes.testimonials')
