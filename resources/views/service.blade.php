d859f60e1747890095382475098075ddee3efecd
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-19 15:05:19 +0800

    html page Services

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -18,7 +18,69 @@
                     </div>
                 </div>
             </div>
-
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas dignissimos consequatur, est nemo pariatur similique expedita, blanditiis alias iusto sapiente nesciunt voluptates, laborum ducimus odit. Sequi similique, consequuntur maxime accusamus, aspernatur voluptates architecto minus soluta facere obcaecati odit? Dolore quaerat magni incidunt explicabo ullam quisquam expedita accusamus id dolores libero!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+				<!-- Single Service Start -->
+				<div class="col-lg-4 text-center">
+					<div class="service-item">
+						<figure>
+						<picture>
+                            <img src="{{ asset('assets/img/home-2-about.png')}}" alt="Car-img" style="width: 100%">
+                        </picture>
+						<figcaption><h3>ПРОКАТ АВТОМОБИЛЕЙ</h3></figcaption>
+						</figure>
+					</div>
+				</div>
+                <div class="col-lg-4 text-center">
+					<div class="service-item">
+						<figure>
+						<picture>
+                            <img src="{{ asset('assets/img/home-2-about.png')}}" alt="Car-img" style="width: 100%">
+                        </picture>
+						<figcaption><h3>ПРОКАТ АВТОМОБИЛЕЙ</h3></figcaption>
+						</figure>
+					</div>
+				</div>
+                <div class="col-lg-4 text-center">
+					<div class="service-item">
+						<figure>
+						<picture>
+                            <img src="{{ asset('assets/img/home-2-about.png')}}" alt="Car-img" style="width: 100%">
+                        </picture>
+						<figcaption><h3>ПРОКАТ АВТОМОБИЛЕЙ</h3></figcaption>
+						</figure>
+					</div>
+				</div>
+                <div class="col-lg-4 text-center">
+					<div class="service-item">
+						<figure>
+						<picture>
+                            <img src="{{ asset('assets/img/home-2-about.png')}}" alt="Car-img" style="width: 100%">
+                        </picture>
+						<figcaption><h3>ПРОКАТ АВТОМОБИЛЕЙ</h3></figcaption>
+						</figure>
+					</div>
+				</div>
+                <div class="col-lg-4 text-center">
+					<div class="service-item">
+						<figure>
+						<picture>
+                            <img src="{{ asset('assets/img/home-2-about.png')}}" alt="Car-img" style="width: 100%">
+                        </picture>
+						<figcaption><h3>ПРОКАТ АВТОМОБИЛЕЙ</h3></figcaption>
+						</figure>
+					</div>
+				</div>
+                
+			</div>
         </div>
     </section>
 
