3424d040f585008ddd8e09b08ce63ee2ab541bba
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-20 20:34:17 +0800

    method create and edit in AdminBrandController

--- app/Http/Controllers/Admin/AdminBrandController.php
+++ app/Http/Controllers/Admin/AdminBrandController.php
@@ -13,67 +13,26 @@ class AdminBrandController extends Controller
         return view('admin.brand.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('admin.brand.form');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(BrandRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Brand  $brand
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Brand $brand)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Brand  $brand
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Brand $brand)
     {
-        //
+        return view('admin.brand.form', compact('brand'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Brand  $brand
-     * @return \Illuminate\Http\Response
-     */
     public function update(BrandRequest $request, Brand $brand)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Brand  $brand
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Brand $brand)
     {
         //
