22861b5e87f0ded3e3b8b4aede4b52574aba08d7
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-20 19:45:56 +0800

    method index in AdminBrandController

--- app/Http/Controllers/Admin/AdminBrandController.php
+++ app/Http/Controllers/Admin/AdminBrandController.php
@@ -8,14 +8,9 @@ use App\Http\Requests\Admin\BrandRequest;
 
 class AdminBrandController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return view('admin.brand.index');
     }
 
     /**
