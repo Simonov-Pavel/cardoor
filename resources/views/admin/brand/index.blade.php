05f1db024bdd190c2e3a3f0def48080b4be1a96f
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-20 19:52:52 +0800

    view brand.index

---
+++ resources/views/admin/brand/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.account_panel')
+@section('title', 'Марки автомобилей')
+@section('panel', "Панель администратора")
+@section('keywords', 'keywords')
+@section('description', 'description')
+
+@section('user_menu')
+  @include('admin.includes.sidebar_menu')
+@endsection
+
+@section('content') 
+<a href="{{ route('brand.create') }}" title='Добавить марку автомобиля' class='btn btn-success mb-3'>Добавить марку автомобиля</a>
+<div class="card">
+    <div class="card-body p-0">
+    
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th>Марка автомобиля</th>
+                    <th style="width: 40px">Деиствие</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($brands as $brand)
+                <tr>
+                    <td class="col-sm-2">{{$brand->title}}</td>
+                    <td class="col-sm-1">
+                        <a href="{{ route('brand.edit', $brand) }}" title='Редактировать'><i class="fa fa-pencil text-success ml-2" aria-hidden="true"></i></a>
+                        <form action="{{ route('brand.destroy', $brand) }}" method="post" class='d-inline-block'>
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="p-0" style="background:transparent; border:none">
+                                <a href="" title='Удалить'><i class="fa fa-times text-danger ml-2" aria-hidden="true"></i></a>
+                            </button>
+                        </form>
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
