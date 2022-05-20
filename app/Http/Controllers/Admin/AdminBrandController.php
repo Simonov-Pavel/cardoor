7e2cf6a8b22b3d4eca9ecb596045bd890a2ec4a7
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-20 19:35:13 +0800

    request BrandRequest and AdmknBrandController

--- app/Http/Controllers/Admin/AdminBrandController.php
+++ app/Http/Controllers/Admin/AdminBrandController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
-use Illuminate\Http\Request;
+use App\Http\Requests\Admin\BrandRequest;
 
 class AdminBrandController extends Controller
 {
@@ -34,7 +34,7 @@ class AdminBrandController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BrandRequest $request)
     {
         //
     }
@@ -68,7 +68,7 @@ class AdminBrandController extends Controller
      * @param  \App\Models\Brand  $brand
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Brand $brand)
+    public function update(BrandRequest $request, Brand $brand)
     {
         //
     }
