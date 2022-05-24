10858de66b6f3133c0bfe38b40ff52113d9ec47a
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-24 19:53:30 +0800

    AdminCarController and CarRequest

---
+++ app/Http/Controllers/Admin/AdminCarController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Car;
+use App\Http\Requests\Admin\CarRequest;
+
+class AdminCarController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(CarRequest $request)
+    {
+        //
+    }
+
+    public function edit(Car $car)
+    {
+        //
+    }
+
+    public function update(CarRequest $request, Car $car)
+    {
+        //
+    }
+
+    public function destroy(Car $car)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/Admin/CarRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'model' => 'required|string',
+            'brithday' => 'required|string',
+            'price' => 'required|numeric',
+            'image' => 'file|image|mimes:jpeg,jpg,png|dimensions:min_width=1280,min_height=800',
+            'brand' => 'required|string|exists:brands,title',
+            'body' => 'required|string|exists:bodies,title',
+            'class' => 'required|string|exists:class_cars,title',
+            'engine' => 'required|string|exists:engines,title',
+            'transmission' => 'required|string|exists:transmissions,title',
+        ];
+    }
+}
