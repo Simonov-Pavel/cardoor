3348fcea527b0b870d89042cb955a0bb2968941b
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-25 18:37:49 +0800

    method create and edit in AdminOptionController

--- app/Http/Controllers/Admin/AdminOptionController.php
+++ app/Http/Controllers/Admin/AdminOptionController.php
@@ -8,77 +8,36 @@ use App\Http\Requests\Admin\OptionRequest;
 
 class AdminOptionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('admin.option.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('admin.option.form');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(OptionRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Option  $option
-     * @return \Illuminate\Http\Response
-     */
     public function show(Option $option)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Option  $option
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Option $option)
     {
-        //
+        return view('admin.option.form', compact('option'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Option  $option
-     * @return \Illuminate\Http\Response
-     */
     public function update(OptionRequest $request, Option $option)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Option  $option
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Option $option)
     {
         //
