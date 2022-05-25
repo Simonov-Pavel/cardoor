4a496abaab888e39f3ba622bb0a4936d03f13f26
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-25 18:12:39 +0800

    link option in sidebar_menu for admin

--- resources/views/admin/includes/sidebar_menu.blade.php
+++ resources/views/admin/includes/sidebar_menu.blade.php
@@ -54,7 +54,7 @@
     </ul>
 </li>
 <li class="nav-item">
-    <a href="#" class="nav-link @menuactive('brand.*') @menuactive('body.*') @menuactive('class.*') @menuactive('engine.*') @menuactive('transmission.*')">
+    <a href="#" class="nav-link @menuactive('car.*') @menuactive('brand.*') @menuactive('body.*') @menuactive('class.*') @menuactive('engine.*') @menuactive('transmission.*') @menuactive('option.*')">
         <i class="nav-icon fa fa-car"></i>
         <p>Автопарк<i class="fas fa-angle-left right"></i></p>
     </a>
@@ -93,6 +93,12 @@
             <a href="{{route('transmission.index')}}" class="nav-link @menuactive('transmission.*')">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Тип коробки передач автомобилей</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{route('option.index')}}" class="nav-link @menuactive('option.*')">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Дополнительные опции автомобилей</p>
             </a>
         </li>
     </ul>
