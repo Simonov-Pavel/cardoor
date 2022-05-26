4efd8f6b206f2e81f9cb8e132d12a8bb50730b62
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-26 20:05:44 +0800

    Отношения модели Transmission с моделью Car

--- app/Models/Transmission.php
+++ app/Models/Transmission.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use App\Models\Car;
 
 class Transmission extends Model
 {
@@ -17,5 +18,10 @@ class Transmission extends Model
         return [
             'slug' => ['source' => 'title']
         ];
+    }
+
+    public function car()
+    {
+        return $this->hasMany(Car::class);
     }
 }
