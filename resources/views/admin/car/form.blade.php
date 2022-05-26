ccbe0ab58847857e887362faa4613b017cc4a440
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-26 21:08:42 +0800

    edit car.form

--- resources/views/admin/car/form.blade.php
+++ resources/views/admin/car/form.blade.php
@@ -58,85 +58,101 @@
         <input type="text"  name='price' required class="form-control" id="price" value="{{ old('price', isset($car)? $car->price : null) }}" placeholder="Добавьте price"> 
         @include('includes.error', ['field'=>'price'])
     </div>
-    @if($brands->count() != 0)
     <div class="form-group">
-        <label>Выбирете модель</label>
-        <select class="form-control" name='brand' required>
+        <label>Выбирете марку автомобиля @if($brands->count() == 0) <a href="{{ route('brand.create') }}">Добавить</a> @endif</label>
+        <select class="form-control" name='brand_id' required>
+            @if($brands->count() != 0)
             <option>{{ old('brand', isset($car)? $car->brand : null) }}</option>
             @foreach($brands as $brand)
-                <option value="{{ $brand->title }}" @if(isset($car) && $car->brand === $brand->title) selected @endif>
+                <option value="{{ $brand->id }}" @if(isset($car) && $car->brand_id === $brand->id) selected @endif>
                     {{$brand->title}}
                 </option>
             @endforeach
+            @else
+                <option>У вас нет выбора марок автомобилей, добавьте по ссылке выше </option>
+            @endif
         </select>
         @include('includes.error', ['field'=>'brand'])
     </div>
-    @endif
-    @if($bodies->count() != 0)
     <div class="form-group">
-        <label>Выбирете тип кузова</label>
-        <select class="form-control" name='body' required>
+        <label>Выбирете тип кузова @if($bodies->count() == 0) <a href="{{ route('body.create') }}">Добавить</a> @endif</label>
+        <select class="form-control" name='body_id' required>
+            @if($bodies->count() != 0)
             <option>{{ old('body', isset($car)? $car->body : null) }}</option>
             @foreach($bodies as $body)
-                <option value="{{ $body->title }}" @if(isset($car) && $car->body === $body->title) selected @endif>
+                <option value="{{ $body->id }}" @if(isset($car) && $car->body_id === $body->id) selected @endif>
                     {{$body->title}}
                 </option>
             @endforeach
+            @else
+                <option>У вас нет выбора кузовов автомобилей, добавьте по ссылке выше </option>
+            @endif
         </select>
         @include('includes.error', ['field'=>'body'])
     </div>
-    @endif
-    @if($clases->count() != 0)
     <div class="form-group">
-        <label>Выбирете класс автомобиля</label>
-        <select class="form-control" name='class' required>
+        <label>Выбирете класс автомобиля @if($clases->count() == 0) <a href="{{ route('class.create') }}">Добавить</a> @endif</label>
+        <select class="form-control" name='class_cars_id' required>
+            @if($clases->count() != 0)
             <option>{{ old('class', isset($car)? $car->class : null) }}</option>
             @foreach($clases as $class)
-                <option value="{{ $class->title }}" @if(isset($car) && $car->class === $class->title) selected @endif>
+                <option value="{{ $class->id }}" @if(isset($car) && $car->class_cars_id === $class->id) selected @endif>
                     {{$class->title}}
                 </option>
             @endforeach
+            @else
+                <option>У вас нет выбора классов автомобилей, добавьте по ссылке выше </option>
+            @endif
         </select>
         @include('includes.error', ['field'=>'class'])
     </div>
-    @endif
-    @if($engines->count() != 0)
     <div class="form-group">
-        <label>Выбирете тип двигателя</label>
-        <select class="form-control" name='engine' required>
+        <label>Выбирете тип двигателя @if($engines->count() == 0) <a href="{{ route('engine.create') }}">Добавить</a> @endif</label>
+        <select class="form-control" name='engine_id' required>
+            @if($engines->count() != 0)
             <option>{{ old('engine', isset($car)? $car->engine : null) }}</option>
             @foreach($engines as $engine)
-                <option value="{{ $engine->title }}" @if(isset($car) && $car->engine === $engine->title) selected @endif>
+                <option value="{{ $engine->id }}" @if(isset($car) && $car->engine_id === $engine->id) selected @endif>
                     {{$engine->title}}
                 </option>
             @endforeach
+            @else
+                <option>У вас нет выбора типов двигателей, добавьте по ссылке выше </option>
+            @endif
         </select>
         @include('includes.error', ['field'=>'engine'])
     </div>
-    @endif
-    @if($transmissions->count() != 0)
     <div class="form-group">
-        <label>Выбирете тип коробки передач</label>
-        <select class="form-control" name='transmission' required>
+        <label>Выбирете тип коробки передач @if($transmissions->count() == 0) <a href="{{ route('transmission.create') }}">Добавить</a> @endif</label>
+        <select class="form-control" name='transmission_id' required>
+            @if($transmissions->count() != 0)
             <option>{{ old('transmission', isset($car)? $car->transmission : null) }}</option>
             @foreach($transmissions as $transmission)
-                <option value="{{ $transmission->title }}" @if(isset($car) && $car->transmission === $transmission->title) selected @endif>
+                <option value="{{ $transmission->id }}" @if(isset($car) && $car->transmission_id === $transmission->id) selected @endif>
                     {{$transmission->title}}
                 </option>
             @endforeach
+            @else
+                <option>У вас нет выбора типов коробки передач, добавьте по ссылке выше </option>
+            @endif
         </select>
         @include('includes.error', ['field'=>'transmission'])
     </div>
-    @endif
+    
     <div class="form-group">
         <label>Дополнительные опции</label>
+        @if($options->count() != 0)
         @foreach($options as $option)
         <div class="form-check">
-            <input name="options[]" value="{{$option->id}}" class="form-check-input" type="checkbox" @if($car->options->where('id', $option->id)->count()) checked @endif>
+            <input name="options[]" value="{{$option->id}}" class="form-check-input" type="checkbox" @if(isset($car) && $car->options->where('id', $option->id)->count()) checked @endif>
             <label class="form-check-label">{{$option->title}}</label>
         </div>
         @endforeach
+        @else
+         <p>У вас нет добавленных доп.опции <a href="{{ route('option.create') }}">Добавить</a></p>
+        @endif
     </div>
+    
     <div class="form-group"> 
         <label for="file">Выбирете изображение @if(isset($car))<small>(если хотите изменить его)</small>@endif</label><br>
         <small>Рекомендуемые минимальные размеры 1280px X 800px</small><br>
