c2ecec8a61497f830d70ce186588068f5bb06509
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-28 14:33:04 +0800

    CarController

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+
+class CarController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    public function show(Car $car)
+    {
+        //
+    }
+}
