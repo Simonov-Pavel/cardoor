61200504d89269cb2bd76683a9ae489727185cad
Simonov-Pavel <simonov.pavel0306@yandex.ru>
2022-05-28 16:00:46 +0800

    get classes and bodies for car.blade

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -35,8 +35,8 @@ class ViewServiceProvider extends ServiceProvider
         View::composer(['admin.service.index', 'service'], 'App\ViewsComposer\ServiceComposer');
         View::composer(['includes.service'], 'App\ViewsComposer\ServiceMainComposer');
         View::composer(['admin.brand.index', 'admin.car.form'], 'App\ViewsComposer\BrandComposer');
-        View::composer(['admin.body.index', 'admin.car.form'], 'App\ViewsComposer\BodyComposer');
-        View::composer(['admin.class.index', 'admin.car.form'], 'App\ViewsComposer\ClassCarComposer');
+        View::composer(['admin.body.index', 'admin.car.form', 'car'], 'App\ViewsComposer\BodyComposer');
+        View::composer(['admin.class.index', 'admin.car.form', 'car'], 'App\ViewsComposer\ClassCarComposer');
         View::composer(['admin.engine.index', 'admin.car.form'], 'App\ViewsComposer\EngineComposer');
         View::composer(['admin.transmission.index', 'admin.car.form'], 'App\ViewsComposer\TransmissionComposer');
         View::composer(['admin.car.index'], 'App\ViewsComposer\CarComposer');
